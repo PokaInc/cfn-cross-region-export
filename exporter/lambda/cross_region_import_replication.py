22387f9b1354432dca50a15f5376ce1b57428d57
Gabriel Pelletier <gpelletier@poka.io>
2019-03-21 09:50:47 -0400

    feat - Group resources by 5 in the depends on to accelerate update (#8)
    * feat - Group resources by 5 in the depends on to accelerate update
    
    * feat - Dynamic group sizing
    
    * feat - Dynamic group sizing

--- exporter/lambda/cross_region_import_replication.py
+++ exporter/lambda/cross_region_import_replication.py
@@ -8,8 +8,8 @@ import botocore
 from raven import Client
 from raven.transport import HTTPTransport
 
-
 MAX_RESOURCES_PER_TEMPLATE = 200
+RESSOURCE_BY_GROUP = 5
 
 
 def lambda_handler(*_):
@@ -41,8 +41,11 @@ def _lambda_handler():
 
     nested_template_urls = []
 
+    number_of_chunk = len(cross_stack_references) / MAX_RESOURCES_PER_TEMPLATE
+    max_group_size = int(max(min(RESSOURCE_BY_GROUP/number_of_chunk, RESSOURCE_BY_GROUP), 1))
+
     for items in _chunks(cross_stack_references, MAX_RESOURCES_PER_TEMPLATE):
-        nested_template_urls.append(_generate_nested_template(items))
+        nested_template_urls.append(_generate_nested_template(items, max_group_size))
 
     master_template_resources = {}
 
@@ -107,9 +110,10 @@ def _build_unsigned_url(template_name):
     )
 
 
-def _generate_nested_template(cross_stack_references):
+def _generate_nested_template(cross_stack_references, max_group_size):
     last_ref_id = None
     ssm_resources = {}
+    resource_count = 0
 
     for ref in cross_stack_references:
         ref_id = _generate_hash(ref['CrossStackRefId'])
@@ -128,7 +132,10 @@ def _generate_nested_template(cross_stack_references):
 
         ssm_resources[ref_id] = ssm_resource
 
-        last_ref_id = ref_id
+        if resource_count % max_group_size == 0:
+            last_ref_id = ref_id
+
+        resource_count += 1
 
     imports_replication_template = {
         'AWSTemplateFormatVersion': '2010-09-09',
